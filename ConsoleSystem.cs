6cc05e2de73723f2cf1637d22a3513286e861f1a
Maxim Bazuev <bazuka5801@mail.ru>
2018-05-20 01:30:32 +0300

    Added caller output in log

--- ConsoleSystem.cs
+++ ConsoleSystem.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 
 namespace SapphireEngine
@@ -13,6 +14,8 @@ namespace SapphireEngine
         public static string OutputPath = "./output.log";
         public static bool IsOutputToFile = true;
 
+        public static bool ShowCallerInLog = false;
+        
         private static Stack<string> ConsoleInputLines = new Stack<string>();
 
         public override void OnUpdate()
@@ -33,26 +36,41 @@ namespace SapphireEngine
             }
         }
 
-        public static void Log(string _format, params object[] _args)
+        private static void Output(string _format, params object[] _args)
         {
-            string line = string.Format($"[{DateTime.Now:HH:mm:ss fff}]: " + _format, _args);
+            string prefix = "";
+            if (ShowCallerInLog)
+            {
+                StackFrame frame = new StackFrame(2, true);
+                var method = frame.GetMethod();
+                var declaringType = method.DeclaringType.Name;
+                if (declaringType.StartsWith("<>"))
+                    declaringType = method.DeclaringType.DeclaringType.Name;
+                prefix = $"[{declaringType}.{method}]";
+            }
+            string line = string.Format($"[{DateTime.Now:HH:mm:ss fff}]{prefix}: " + _format, _args);
             Console.WriteLine(line);
             OnReceivedLog?.Invoke(line);
             if (IsOutputToFile)
                 File.AppendAllText(OutputPath, "\n" + line);
         }
 
+        public static void Log(string _format, params object[] _args)
+        {
+            Output(_format, _args);
+        }
+
         public static void LogWarning(string _format, params object[] _args)
         {
             Console.ForegroundColor = ConsoleColor.Yellow;
-            Log(_format, _args);
+            Output(_format, _args);
             Console.ResetColor();
         }
 
         public static void LogError(string _format, params object[] _args)
         {
             Console.ForegroundColor = ConsoleColor.Red;
-            Log(_format, _args);
+            Output(_format, _args);
             Console.ResetColor();
         }
     }
